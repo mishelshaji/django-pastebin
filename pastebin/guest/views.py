39fb0d3995e567873e38cd04b7ff39d16222b4ab
Mishel <mishelvettukattil@outlook.com>
2020-09-24 13:00:50 +0530

    Added about page route

--- pastebin/guest/views.py
+++ pastebin/guest/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render, HttpResponse
 # Create your views here.
 def home(request):
     return HttpResponse("Hello World")
+
+def about(request):
+    return HttpResponse("About page")
