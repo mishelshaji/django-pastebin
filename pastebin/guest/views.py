d59b934e416cf57227b05fd817da501dbf1308b6
Mishel <mishelvettukattil@outlook.com>
2020-09-25 12:15:12 +0530

    Render html page

--- pastebin/guest/views.py
+++ pastebin/guest/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, HttpResponse
 
 # Create your views here.
 def home(request):
-    return HttpResponse("Hello World")
+    return render(request, 'home.html')
 
 def about(request):
     return HttpResponse("About page")
